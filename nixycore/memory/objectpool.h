8d548a90de99287312013719f46fa4dc954ac5eb
mutouyun <mark.lonr@tom.com>
2014-02-08 14:16:24 +0900

    object_pool将storage与pool分离

--- nixycore/memory/objectpool.h
+++ nixycore/memory/objectpool.h
@@ -24,6 +24,97 @@
 NX_BEG
 //////////////////////////////////////////////////////////////////////////
 
+/*
+    the data structure for storing objects
+*/
+
+template <typename T, class FixedAlloc_>
+class object_pool_storage
+{
+public:
+    typedef T type_t;
+
+protected:
+    FixedAlloc_ allocator_;
+    functor<type_t*(pvoid)> constructor_;
+
+    struct alloc_t
+    {
+        nx::byte data_[sizeof(type_t)];
+        alloc_t* next_;
+    };
+
+    alloc_t* free_head_;
+    alloc_t* free_tail_;
+
+    size_t size_;
+
+public:
+    template <typename F>
+    explicit object_pool_storage(const F& f)
+        : allocator_(sizeof(alloc_t))
+        , constructor_(f)
+        , free_head_(nx::nulptr)
+        , free_tail_(nx::nulptr)
+        , size_(0)
+    {}
+
+    ~object_pool_storage()
+    {
+        while (!is_empty()) destroy(take());
+    }
+
+public:
+    type_t* create(void)
+    {
+        alloc_t* p = static_cast<alloc_t*>(allocator_.alloc());
+        nx_assert(p);
+        return constructor_(p->data_);
+    }
+
+    void destroy(type_t* objc)
+    {
+        nx_assert(objc);
+        nx_destruct(objc, type_t);
+        allocator_.free(objc);
+    }
+
+    type_t* take(void)
+    {
+        nx_assert(free_head_);
+        alloc_t* p = free_head_;
+        free_head_ = free_head_->next_;
+        if (free_tail_ == p)
+            free_tail_ = free_head_ = nx::nulptr;
+        -- size_;
+        return reinterpret_cast<type_t*>(p->data_);
+    }
+
+    void put(type_t* objc)
+    {
+        nx_assert(objc);
+        alloc_t* p = reinterpret_cast<alloc_t*>(objc);
+        if ( free_tail_) free_tail_->next_ = p;
+        free_tail_ = p;
+        if (!free_head_) free_head_ = free_tail_;
+        ++ size_;
+    }
+
+    size_t size(void) const
+    {
+        return size_;
+    }
+
+    bool is_empty(void) const
+    {
+        return (size_ == 0);
+    }
+};
+
+/*
+    the function templates for constructing object
+*/
+
 namespace private_object_pool
 {
     template <typename T>
@@ -42,96 +133,51 @@ namespace private_object_pool
 #undef NX_OBJECT_POOL_HELPER_
 }
 
-template <typename T, class FixedAlloc_ = fixed_pool<> >
+/*
+    the object pool
+*/
+
+template
+<
+    typename T,
+    class FixedAlloc_ = fixed_pool<>,
+    template <typename, class>
+    class Storage_ = object_pool_storage
+>
 class object_pool : NonCopyable
 {
+    typedef Storage_<T, FixedAlloc_> storage_t;
+
 public:
-    typedef T type_t;
+    typedef typename storage_t::type_t type_t;
 
-private:
-    FixedAlloc_ allocator_;
-
-    functor<type_t*(pvoid)> constructor_;
-
-    struct alloc_t
-    {
-        nx::byte data_[sizeof(type_t)];
-        alloc_t* next_;
-    };
-
-    alloc_t* free_head_;
-    alloc_t* free_tail_;
+protected:
+    storage_t storage_;
 
     size_t min_size_;
     size_t max_size_;
-    size_t size_;
 
 public:
     object_pool(size_t min_sz = 0, size_t max_sz = (size_t)~0)
-        : allocator_(sizeof(alloc_t))
-        , free_head_(nx::nulptr)
-        , free_tail_(nx::nulptr)
+        : storage_(static_cast<type_t*(*)(pvoid)>(&private_object_pool::construct<type_t>))
         , min_size_(0)
         , max_size_(0)
-        , size_(0)
     {
-        constructor_ = static_cast<type_t*(*)(pvoid)>(&private_object_pool::construct<type_t>);
         limit(min_sz, max_sz);
     }
 
 #define NX_OBJECT_POOL_(n) \
     template <NX_PP_TYPE_1(n, typename P)> \
     object_pool(NX_PP_TYPE_2(n, P, par), size_t min_sz = 0, size_t max_sz = (size_t)~0) \
-        : allocator_(sizeof(alloc_t)) \
-        , free_head_(nx::nulptr) \
-        , free_tail_(nx::nulptr) \
+        : storage_(bind(&private_object_pool::construct<type_t, NX_PP_TYPE_1(n, P)>, \
+                                                          _1, NX_PP_TYPE_1(n, par))) \
         , min_size_(0) \
         , max_size_(0) \
-        , size_(0) \
     { \
-        constructor_ = bind(&private_object_pool::construct<type_t, NX_PP_TYPE_1(n, P)>, \
-                                                                _1, NX_PP_TYPE_1(n, par)); \
         limit(min_sz, max_sz); \
     }
     NX_PP_MULT_MAX(NX_OBJECT_POOL_)
 #undef NX_OBJECT_POOL_
-
-    ~object_pool()
-    {
-        while(free_head_) destroy();
-    }
-
-private:
-    type_t* create(void)
-    {
-        alloc_t* p = static_cast<alloc_t*>(allocator_.alloc());
-        nx_assert(p);
-        p->next_ = nx::nulptr;
-        return constructor_(p->data_);
-    }
-
-    void destroy(void)
-    {
-        nx_assert(free_head_);
-        alloc_t* p = free_head_;
-        free_head_ = free_head_->next_;
-        -- size_;
-        nx_destruct(reinterpret_cast<type_t*>(p), type_t);
-        allocator_.free(p);
-    }
-
-    type_t* pick(void)
-    {
-        nx_assert(free_head_);
-        alloc_t* p = free_head_;
-        free_head_ = free_head_->next_;
-        if (free_head_)
-            p->next_ = nx::nulptr;
-        else
-            free_tail_ = free_head_;
-        -- size_;
-        return reinterpret_cast<type_t*>(p->data_);
-    }
 
 public:
     void limit(size_t min_sz = 0, size_t max_sz = (size_t)~0)
@@ -140,60 +186,86 @@ public:
         nx_assert(min_sz <= max_sz);
         min_size_ = min_sz;
         max_size_ = max_sz;
-        if (size_ > max_size_)
+        if (storage_.size() > max_size_)
         {
             do
             {
-                destroy();
+                nx_verify(decrease());
             }
-            while (size_ > max_size_);
+            while (storage_.size() > max_size_);
         }
         else
         {
-            while (size_ < min_size_)
+            while (storage_.size() < min_size_)
             {
-                free(create());
+                nx_verify(increase());
             }
         }
     }
 
-    size_t min_size(void) const { return min_size_; }
-    size_t max_size(void) const { return max_size_; }
+    size_t min_size(void) const
+    {
+        return min_size_;
+    }
 
-    size_t size(void) const { return size_; }
+    size_t max_size(void) const
+    {
+        return max_size_;
+    }
+
+    size_t size(void) const
+    {
+        return storage_.size();
+    }
+
+    bool is_empty(void) const
+    {
+        return storage_.is_empty();
+    }
+
+    bool is_full(void) const
+    {
+        return (storage_.size() >= max_size_);
+    }
+
+    bool is_thin(void) const
+    {
+        return (storage_.size() <= min_size_);
+    }
 
 public:
+    bool increase(void)
+    {
+        if (is_full()) return false;
+        storage_.put(storage_.create());
+        return true;
+    }
+
+    bool decrease(void)
+    {
+        if (is_thin()) return false;
+        storage_.destroy(storage_.take());
+        return true;
+    }
+
     type_t* alloc(void)
     {
-        if(!free_head_) free(create());
-        return pick();
+        if (is_empty()) increase();
+        return storage_.take();
     }
 
     void free(type_t* objc)
     {
         if (!objc) return;
-        alloc_t* p = reinterpret_cast<alloc_t*>(objc);
-        nx_assert(!(p->next_));
-        if (p->next_) return;
-        if (size_ < max_size_)
-        {
-            if (free_tail_)
-                free_tail_->next_ = p;
-            free_tail_ = p;
-            if (!free_head_) free_head_ = free_tail_;
-            ++ size_;
-        }
+        if (is_full())
+            storage_.destroy(objc);
         else
-        {
-            nx_destruct(objc, type_t);
-            allocator_.free(p);
-        }
+            storage_.put(objc);
     }
 
     void clear(void)
     {
-        while(free_head_ && (size_ > min_size_)) destroy();
-        if (!free_head_) free_tail_ = free_head_;
+        while (decrease()) continue;
     }
 };
 
