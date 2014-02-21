859e8bdec956de5cbb1bebff316b884abefc462f
mutouyun <mark.lonr@tom.com>
2014-02-22 00:18:32 +0900

    统一文件的版权说明格式

--- nixycore/container/array.h
+++ nixycore/container/array.h
@@ -1,12 +1,11 @@
 /*
+    The Nixy Library
+    Code covered by the MIT License
+
     Modified from The Boost Library
-    Modified by : 2013 darkcat
+    Modified by : mutouyun (http://darkc.at)
 
     (C) Copyright Nicolai M. Josuttis 2001.
-
-    Distributed under the Boost Software License, Version 1.0. (See
-    accompanying file LICENSE_1_0.txt or copy at
-    http://www.boost.org/LICENSE_1_0.txt)
 */
 
 #pragma once
--- nixycore/typemanip/typelist.h
+++ nixycore/typemanip/typelist.h
@@ -1,19 +1,11 @@
 /*
+    The Nixy Library
+    Code covered by the MIT License
+
     Modified from The Loki Library
-    Modified by : 2013 darkcat
+    Modified by : mutouyun (http://darkc.at)
 
     Copyright (c) 2001 by Andrei Alexandrescu
-
-    This code accompanies the book:
-    Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design 
-        Patterns Applied". Copyright (c) 2001. Addison-Wesley.
-    Permission to use, copy, modify, distribute and sell this software for any 
-        purpose is hereby granted without fee, provided that the above copyright 
-        notice appear in all copies and that both that copyright notice and this 
-        permission notice appear in supporting documentation.
-    The author or Addison-Welsey Longman make no representations about the 
-        suitability of this software for any purpose. It is provided "as is" 
-        without express or implied warranty.
 */
 
 #pragma once
