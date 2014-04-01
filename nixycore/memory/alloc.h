074902a1d68e30ae139a55647cb3f3cae6039581
mutouyun <mark.lonr@tom.com>
2014-04-01 22:22:39 +0900

    Add nx::realloc
    添加全局的realloc（支持析构掉已存在的指针，但不支持重新构造）

--- nixycore/memory/alloc.h
+++ nixycore/memory/alloc.h
@@ -32,32 +32,32 @@ inline pvoid alloc(size_t size)
 
 namespace private_alloc
 {
-    template <typename Alloc_, typename T>
+    template <typename AllocT, typename T>
     struct detail
     {
         static T* alloc(void)
         {
-            return nx_construct(Alloc_::alloc(sizeof(T)), T);
+            return nx_construct(AllocT::alloc(sizeof(T)), T);
         }
 
 #   define NX_ALLOC_(n) \
         template <NX_PP_TYPE_1(n, typename P)> \
         static T* alloc(NX_PP_TYPE_2(n, P, par)) \
         { \
-            return nx_construct(Alloc_::alloc(sizeof(T)), T, NX_PP_TYPE_1(n, par)); \
+            return nx_construct(AllocT::alloc(sizeof(T)), T, NX_PP_TYPE_1(n, par)); \
         }
         NX_PP_MULT_MAX(NX_ALLOC_)
 #   undef NX_ALLOC_
     };
 
     /* Make array to array pointer */
-    template <typename Alloc_, typename T_, size_t N>
-    struct detail<Alloc_, T_[N]>
+    template <typename AllocT, typename T_, size_t N>
+    struct detail<AllocT, T_[N]>
     {
         typedef T_ T[N];
         static T* alloc(void)
         {
-            T* p = (T*)Alloc_::alloc(sizeof(T));
+            T* p = (T*)AllocT::alloc(sizeof(T));
             nx_construct_arr(*p, T_, N);
             return p;
         }
@@ -66,7 +66,7 @@ namespace private_alloc
         template <NX_PP_TYPE_1(n, typename P)> \
         static T* alloc(NX_PP_TYPE_2(n, P, par)) \
         { \
-            T* p = (T*)Alloc_::alloc(sizeof(T)); \
+            T* p = (T*)AllocT::alloc(sizeof(T)); \
             nx_construct_arr(*p, T_, N, NX_PP_TYPE_1(n, par)); \
             return p; \
         }
@@ -74,12 +74,12 @@ namespace private_alloc
 #   undef NX_ALLOC_
     };
 
-    template <typename Alloc_>
-    struct detail<Alloc_, void>
+    template <typename AllocT>
+    struct detail<AllocT, void>
     {
         static void* alloc(size_t size)
         {
-            return Alloc_::alloc(size);
+            return AllocT::alloc(size);
         }
     };
 }
@@ -90,10 +90,10 @@ inline T* alloc(void)
     return private_alloc::detail<NX_DEFAULT_ALLOC, T>::alloc();
 }
 
-template <typename Alloc_, typename T>
+template <typename AllocT, typename T>
 inline T* alloc(void)
 {
-    return private_alloc::detail<Alloc_, T>::alloc();
+    return private_alloc::detail<AllocT, T>::alloc();
 }
 
 #define NX_ALLOC_(n) \
@@ -102,10 +102,10 @@ inline T* alloc(NX_PP_TYPE_2(n, P, par)) \
 { \
     return private_alloc::detail<NX_DEFAULT_ALLOC, T>::alloc(NX_PP_TYPE_1(n, par)); \
 } \
-template <typename Alloc_, typename T, NX_PP_TYPE_1(n, typename P)> \
+template <typename AllocT, typename T, NX_PP_TYPE_1(n, typename P)> \
 inline T* alloc(NX_PP_TYPE_2(n, P, par)) \
 { \
-    return private_alloc::detail<Alloc_, T>::alloc(NX_PP_TYPE_1(n, par)); \
+    return private_alloc::detail<AllocT, T>::alloc(NX_PP_TYPE_1(n, par)); \
 }
 NX_PP_MULT_MAX(NX_ALLOC_)
 #undef NX_ALLOC_
@@ -119,10 +119,10 @@ inline void free(pvoid p)
     NX_DEFAULT_ALLOC::free(p);
 }
 
-template <typename Alloc_>
+template <typename AllocT>
 inline void free(pvoid p)
 {
-    Alloc_::free(p);
+    AllocT::free(p);
 }
 
 template <typename T>
@@ -133,12 +133,12 @@ inline void free(T* p)
     NX_DEFAULT_ALLOC::free(p);
 }
 
-template <typename Alloc_, typename T>
+template <typename AllocT, typename T>
 inline void free(T* p)
 {
     if (!p) return;
     nx_destruct(p, T);
-    Alloc_::free(p);
+    AllocT::free(p);
 }
 
 template <typename T, size_t N>
@@ -149,12 +149,41 @@ inline void free(T(* p)[N])
     NX_DEFAULT_ALLOC::free(p);
 }
 
-template <typename Alloc_, typename T, size_t N>
+template <typename AllocT, typename T, size_t N>
 inline void free(T(* p)[N])
 {
     if (!p) return;
     nx_destruct_arr(*p, T, N);
-    Alloc_::free(p);
+    AllocT::free(p);
+}
+
+/*
+    realloc
+*/
+
+inline pvoid realloc(pvoid p, size_t size)
+{
+    return NX_DEFAULT_ALLOC::realloc(p, size);
+}
+
+template <typename AllocT>
+inline pvoid realloc(pvoid p, size_t size)
+{
+    return AllocT::realloc(p, size);
+}
+
+template <typename T>
+inline pvoid realloc(T* p, size_t size)
+{
+    if (p) nx_destruct(p, T);
+    return NX_DEFAULT_ALLOC::realloc(p, size);
+}
+
+template <typename AllocT, typename T>
+inline pvoid realloc(T* p, size_t size)
+{
+    if (p) nx_destruct(p, T);
+    return AllocT::realloc(p, size);
 }
 
 //////////////////////////////////////////////////////////////////////////
